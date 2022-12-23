147fde843fe7b678dce6f5698df99525e4be10c3
Gundu Rakesh <113242509+GunduRakesh@users.noreply.github.com>
2022-12-23 13:20:40 +0530

    Update hdfclogin.java
    pullreq

--- hdfclogin.java
+++ hdfclogin.java
@@ -1,3 +1,6 @@
 This is hdfc login page using java program....
 adding line in the development branch1
 This rakesh 
+
+pull requests is the process of integrating branches in the remote repo or github
+rakesh is creating pull req
