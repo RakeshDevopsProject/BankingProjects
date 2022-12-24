1b9bc2ef05c874ed61a4b34e7af5952cc8dc9441
vinaysgithub <vinayyenaganti@hotmail.com>
2022-12-24 13:04:43 +0530

    mainte commit

--- citibank.sh
+++ citibank.sh
@@ -3,3 +3,4 @@ This is citibank details providing branch which is used for customers logins
 this line updated in master branch
 this line is added in stage branch
 this line added by vinay
+this line added as maintainer
